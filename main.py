1f14c98fe0ddbdf84b8b07d03b7d48c2b06136ad
Kenneth Strawn <kstrawn0@saddleback.edu>
2026-06-20 11:07:09 -0700

    Document graceful exit from the C2 shell

--- main.py
+++ main.py
@@ -160,7 +160,9 @@ def main():
             elif '/interact' in user_command and any(sid in user_command for sid in SESSIONS) and SELECTED_SESSION == '':
                 session_id = next(sid in user_command for sid in SESSIONS)
                 _ = interact(session_id)
-            elif '/exit' in user_command:
+
+            # Exit gracefully if user types `/exit`
+            if '/exit' in user_command:
                 break
 
             # Skip to next loop iteration if user doesn't type anything
